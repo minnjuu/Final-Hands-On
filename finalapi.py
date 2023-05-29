b1659b32be60d2e8814b00e66cb1b71edb1080e5
Nick Francis Bundal <nnikeetmg@gmail.com>
2023-05-29 15:57:25 +0800

    test northwind database

--- finalapi.py
+++ finalapi.py
@@ -1,30 +1,42 @@
-from flask import Flask, render_template, request 
+
+from flask import Flask, make_response, jsonify, request
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = "seagate"
+app.config["MYSQL_DB"] = "northwind"
 
-
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'root'
-app.config['MYSQL_DB'] = 'MyDB'
+app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 
 mysql = MySQL(app)
 
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == "POST":
-        details = request.form
-        firstName = details['fname']
-        lastName = details['lname']
-        cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO MyUsers(firstName, lastName) VALUES (%s, %s)", (firstName, lastName))
-        mysql.connection.commit()
-        cur.close()
-        return 'success'
-    return render_template('index.html')
+def data_fetch(query):
+    cur = mysql.connection.cursor()
+    cur.execute(query)
+    data = cur.fetchall()
+    cur.close()
+    # Convert bytes to strings
+    data = [{k: v.decode() if isinstance(v, bytes) else v for k, v in item.items()} for item in data]
+    return data
 
 
-if __name__ == '__main__':
-       app.run()
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+
+@app.route("/customers", methods=["GET"])
+def get_customers():
+    cur = mysql.connection.cursor()
+    query = """select * from customers;"""
+    cur.execute(query)
+    data = data_fetch(query)
+    cur.close()
+    return make_response(jsonify(data), 200)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
