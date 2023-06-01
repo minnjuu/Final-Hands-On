e170d6312ccda1bd23da712eb69dcec21ba0ca61
Nick Francis Bundal <nnikeetmg@gmail.com>
2023-06-01 13:01:09 +0800

    added create, retrieve, update and delete options in client batch file

--- client.bat
+++ client.bat
@@ -1,17 +1,69 @@
 @echo off
-
 setlocal enabledelayedexpansion
-goto delete
+
+curl  http://127.0.0.1:5000/
+
+choice /c 1234e /N
+
+if %ERRORLEVEL% == 1 (
+	cls
+	goto add
+)
+if %ERRORLEVEL% == 2 (
+	cls
+	goto retrieve
+)
+if %ERRORLEVEL% == 3 (
+	cls
+	goto update
+)
+if %ERRORLEVEL% == 4 (
+	cls
+	goto delete
+)
+if %ERRORLEVEL% == 5 (
+	cls
+	goto end
+)
+
+goto end
+
+:retrieve
 set /p "city=city: "
 
-REM Replace spaces with %20
 set "encodedCity=!city: =%%20!"
-
 curl -X GET "http://127.0.0.1:5000/customers/'!encodedCity!'"
-
+goto end
 :add
-curl -X POST -H "Content-Type: application/json" -d "{\"company\":\"Company ABC\", \"first_name\":\"Kabado\", \"last_name\":\"Bente\", \"job_title\":\"Purchasing Manager\", \"address\":\"haduken street\", \"city\":\"Miami\"}" http://127.0.0.1:5000/customers
+set /p "company=Enter company name: "
+set /p "first_name=Enter first name: "
+set /p "last_name=Enter last name: "
+set /p "job_title=Enter job title: " 
+set /p "address=Enter address: " 
+set /p "city=Enter city: "
 
+set "json_data={\"company\":\"%company%\",\"first_name\":\"%first_name%\",\"last_name\":\"%last_name%\",\"job_title\":\"%job_title%\",\"address\":\"%address%\",\"city\":\"%city%\"}"
+
+curl -X POST -H "Content-Type: application/json" -d "%json_data%" http://127.0.0.1:5000/customers
+goto end
 :delete
-curl -X DELETE http://127.0.0.1:5000/customers/31
+set /p "d_id=Enter Customer ID: "
+curl -X DELETE http://127.0.0.1:5000/customers/%d_id%
+goto end
+
+:update
+set /p "u_id=Enter Customer ID: "
+echo Enter Customer Details for Updating
+set /p "company=Enter company name: "
+set /p "first_name=Enter first name: "
+set /p "last_name=Enter last name: "
+set /p "job_title=Enter job title: " 
+set /p "address=Enter address: " 
+set /p "city=Enter city: "
+
+set "json_data={\"company\":\"%company%\",\"first_name\":\"%first_name%\",\"last_name\":\"%last_name%\",\"job_title\":\"%job_title%\",\"address\":\"%address%\",\"city\":\"%city%\"}"
+
+curl -X PUT -H "Content-Type: application/json" -d "%json_data%" http://127.0.0.1:5000/customers/%u_id%
+
+:end
 pause
