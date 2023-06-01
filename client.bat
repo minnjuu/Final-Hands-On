a2d1a9d5af65c8f15935140ce062e013f345b26f
Nick Francis Bundal <nnikeetmg@gmail.com>
2023-06-01 17:31:43 +0800

    add optionality in client batch file

--- client.bat
+++ client.bat
@@ -29,11 +29,43 @@ if %ERRORLEVEL% == 5 (
 goto end
 
 :retrieve
-set /p "city=city: "
+echo SELECT OPERATION
+echo [1] Retrieve ALL Customers        
+echo [2] Search Customer 
+echo [3] Show Customer Orders     
+echo [4] Show Customers by City
+
+choice /c 1234 /N
+
+if %ERRORLEVEL% == 1 (
+	cls
+	goto all
+)
+if %ERRORLEVEL% == 2 (
+	cls
+	goto search
+)
+if %ERRORLEVEL% == 3 (
+	cls
+	goto orders
+)
+if %ERRORLEVEL% == 4 (
+	cls
+	goto city
+)
+:city
+set /p "city=Enter city: "
+rem Validate input (city cannot be empty)
+if "%city%"=="" (
+    echo City cannot be empty
+    pause
+    goto retrieve
+)
 
 set "encodedCity=!city: =%%20!"
 curl -X GET "http://127.0.0.1:5000/customers/'!encodedCity!'"
 goto end
+
 :add
 set /p "company=Enter company name: "
 set /p "first_name=Enter first name: "
@@ -42,24 +74,89 @@ set /p "job_title=Enter job title: "
 set /p "address=Enter address: " 
 set /p "city=Enter city: "
 
+rem Validate input (company, first_name, last_name cannot be empty)
+if "%company%"=="" (
+    echo Company name cannot be empty
+    pause
+    goto add
+)
+if "%first_name%"=="" (
+    echo First name cannot be empty
+    pause
+    goto add
+)
+if "%last_name%"=="" (
+    echo Last name cannot be empty
+    pause
+    goto add
+)
+
 set "json_data={\"company\":\"%company%\",\"first_name\":\"%first_name%\",\"last_name\":\"%last_name%\",\"job_title\":\"%job_title%\",\"address\":\"%address%\",\"city\":\"%city%\"}"
 
 curl -X POST -H "Content-Type: application/json" -d "%json_data%" http://127.0.0.1:5000/customers
 goto end
+
 :delete
 set /p "d_id=Enter Customer ID: "
+if "%d_id%"=="" (
+    echo Customer ID cannot be empty
+    pause
+    goto update
+)
+set /a valid_did=%d_id%
+if %d_id% EQU %valid_did% (
+    goto del
+) else (
+    echo Invalid Customer ID
+    pause
+    goto delete
+)
+:del
 curl -X DELETE http://127.0.0.1:5000/customers/%d_id%
 goto end
 
 :update
 set /p "u_id=Enter Customer ID: "
+if "%u_id%"=="" (
+    echo Customer ID cannot be empty
+    pause
+    goto update
+)
+set /a valid_uid=%u_id%
+if %u_id% EQU %valid_uid% (
+    goto u_details
+) else (
+    echo Invalid Customer ID
+    pause
+    goto update
+)
 echo Enter Customer Details for Updating
+:u_details
 set /p "company=Enter company name: "
 set /p "first_name=Enter first name: "
 set /p "last_name=Enter last name: "
 set /p "job_title=Enter job title: " 
 set /p "address=Enter address: " 
 set /p "city=Enter city: "
+
+rem Validate input (u_id, company, first_name, last_name cannot be empty)
+
+
+if "%company%"=="" (
+    echo Company name cannot be empty
+    pause
+    goto u_details
+)
+if "%first_name%"=="" (
+    echo First name cannot be empty
+    pause
+    goto u_details
+)
+if "%last_name%"=="" (
+    echo Last name cannot be empty
+    pause
+    goto u_details
+)
 
 set "json_data={\"company\":\"%company%\",\"first_name\":\"%first_name%\",\"last_name\":\"%last_name%\",\"job_title\":\"%job_title%\",\"address\":\"%address%\",\"city\":\"%city%\"}"
 
